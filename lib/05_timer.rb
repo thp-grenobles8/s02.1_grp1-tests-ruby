6df3292e80a1369f8b42fec080703fb473e345cf
Hazrian <chazotflorian@gmail.com>
2019-04-08 15:59:02 +0200

    test programme

--- lib/05_timer.rb
+++ lib/05_timer.rb
@@ -3,6 +3,6 @@
 #   2,"0")}:#{(time%60).to_s.rjust(2,"0")}"
 # end
 
-def time_string(string)
-    Time.at(string).utc.strftime("%H:%M:%S")
+def time_string(time)
+    Time.at(time).utc.strftime("%H:%M:%S")
 end
